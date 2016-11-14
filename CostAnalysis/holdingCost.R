98edf0cdc11feff0fbe37c69ea3e06b1fefeccfc
Patrick Talley <talleyp@github.com>
2016-11-14 18:27:38 -0500

    More cost curve analysis: Fixed holding cost, compared change and current

--- CostAnalysis/holdingCost.R
+++ CostAnalysis/holdingCost.R
@@ -3,28 +3,32 @@
 # Use this multiply by assumed cost of $700,000 by area of a gaylord (48x48 in^2)
 
 rotate <- function(x) apply(t(x),1,rev)
-# totalArea <- 1884*4124
+totalArea <- 1884*4124
 fCost <- 700000
 gArea <- 48*48
 
 ## Load data
 nochange <- as.matrix(read.table("costNoChange.csv", sep=","))
-nochange <- rotate(nochange)
+#nochange <- rotate(nochange)
 noNorm <- nochange / sum(nochange) #Normalize
 
 flip <- as.matrix(read.table("costFlipWS.csv", sep=","))
-flip <- rotate(flip)
+#flip <- rotate(flip)
 flipNorm <- flip / sum(flip)
 
 fullchange <- as.matrix(read.table("costFullChange.csv", sep=","))
-fullchange <- rotate(fullchange)
+#fullchange <- rotate(fullchange)
 fullNorm <- fullchange / sum(fullchange)
 
 # TV areas
-tvNochangeRow <- c(sum(328,326,326,327,36):sum(328,326,326,372,394),
-                   1:328)
-tvNochangeCol <- c(sum(205,233,208):sum(205,233,233,277,265,265), 
-                   sum(205,233,233,277,265,265):sum(205,233,233,277,265,265,281,286,282,289,288,285))
+tvNochangeRow <- c((48*21):(48*29),1:(6*48))
+tvNochangeCol <- c((30*48):(62*48))
 
-noChangeCost <- max(noNorm[tvNochangeRow,tvNochangeCol]) * fCost * gArea / 365
-## $2.36 / (year*gaylord)
+tvFullRow <- c(1:(48*3), 1:(48*16))
+tvFullCol <- c((30*48):(62*48),(10*48):(26*48))
+
+noChangeCost <- mean(tail(sort(noNorm[tvNochangeRow,tvNochangeCol]),50)) * fCost * gArea / 365 
+## $31.89 / (year*gaylord)
+
+fullCost <- mean(tail(sort(fullNorm[tvNochangeRow,tvNochangeCol]),50)) * fCost * gArea / 365 
+## 12.02 / (year*gaylord)
